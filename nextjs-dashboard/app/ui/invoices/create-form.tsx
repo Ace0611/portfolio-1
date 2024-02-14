f3ed6b1e5600e490b3a5cdb365c6668b87f2d6e7
Mayur Ray <shiremen@gmail.com>
2024-02-15 00:27:41 +0530

    Metadata additions

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -11,11 +11,16 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Create invoices',
+}
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
-  
+
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
